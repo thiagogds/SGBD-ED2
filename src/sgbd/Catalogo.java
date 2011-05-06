4e386907aea65a01d5c8dcd302f7bb7833196865
Thiago Garcia <thiagogds14@gmail.com>
2011-05-06 19:12:50 -0300

    Implementa a criação do arquivo com uma tabela

--- src/sgbd/Catalogo.java
+++ src/sgbd/Catalogo.java
@@ -1,5 +1,8 @@
 package sgbd;
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -8,8 +11,25 @@ import java.util.HashMap;
  */
 public class Catalogo {
 
+    private String nome;
+    private HashMap<String, String> atributos;
+
     public Catalogo(String nome, HashMap<String, String> atributos) {
-        return;
+        this.nome = nome;
+        this.atributos = atributos;
     }
 
+    public void salvaTabela() throws IOException {
+        DataOutputStream out = new DataOutputStream(new FileOutputStream("tabelas.dat"));
+        try {
+            out.writeUTF(this.nome);
+            for (String key : atributos.keySet()) {
+                out.writeUTF(key);
+                out.writeUTF(atributos.get(key));
+            }
+        } catch (Exception e) {
+        } finally {
+            out.close();
+        }
+    }
 }
--- test/TestCatalogo.java
+++ test/TestCatalogo.java
@@ -1,9 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import org.junit.Before;
 import org.junit.Assert;
 import org.junit.Test;
 import sgbd.Catalogo;
-
+import util.Arquivos;
 /**
  *
  * @author Grupo 1
@@ -26,5 +29,24 @@ public class TestCatalogo extends Assert {
         Catalogo tabela = new Catalogo(nome, atributos);
         
         assertNotNull(null, tabela);
+    }
+
+    @Test
+    public void testeCriaPrimeiroArquivoCatalogoComOsAtributosCorretos() throws FileNotFoundException, IOException{
+        String nome = "Cliente";
+        HashMap<String, String> atributos = new HashMap<String, String>();
+        atributos.put("Nome", "String");
+        atributos.put("Idade", "Integer");
+
+        Catalogo tabela = new Catalogo(nome, atributos);
+        tabela.salvaTabela();
+        
+        ArrayList<String> arrTabela = Arquivos.leTabela("tabelas.dat");
+
+        assertTrue(arrTabela.contains("Cliente"));
+        assertTrue(arrTabela.contains("Nome"));
+        assertTrue(arrTabela.contains("Idade"));
+        assertTrue(arrTabela.contains("String"));
+        assertTrue(arrTabela.contains("Integer"));
     }
 }
